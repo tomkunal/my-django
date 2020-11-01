5f62e184f837a5d18cb31cc6ec96340e701f8646
tomkunal <73659893+tomkunal@users.noreply.github.com>
2020-11-01 22:48:19 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.addTodo, name='add'),
     path('time',views.timekunal),
+    path('rolltime',views.timeroll),
     path('Game',views.render_game),
     path('complete/<todo_id>', views.completeTodo, name='complete'),
     path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
